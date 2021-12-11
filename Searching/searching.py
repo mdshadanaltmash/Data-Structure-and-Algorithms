f9f2c463868c7b65125e162e1a456ba15ab14816
mdshadanaltmash <mdshadanaltmash44@gmail.com>
2021-12-11 22:07:58 +0530

    LeetCode Practice

--- Searching/searching.py
+++ Searching/searching.py
@@ -21,6 +21,26 @@ def binary_search(arr, val) -> int:
     else:
         return -1
 
+def leet_code(nums, target):
+    start = 0
+    end =len(nums)-1
+    mid = (start+end)//2
+    while not(nums[mid]==target) and start<end:
+        if target<nums[mid]:
+            end = mid
+        else:
+            start= mid+1
+        mid =(start+end)//2
+    if nums[mid]==target:
+        return mid
+    else:
+        if target>nums[mid]:
+            return mid+1
+        else:
+            return mid
+
 array = [1,2,4,7,9,12,14,17,18]
+array=[1,3]
 print(linear_search(array,9))
 print(binary_search(array, 14))
+print(leet_code(array,0))
