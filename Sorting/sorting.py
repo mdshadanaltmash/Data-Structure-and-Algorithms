109d7bc556ec60c0422ad2c22f158ed2760d1508
mdshadanaltmash <mdshadanaltmash44@gmail.com>
2021-12-14 00:32:59 +0530

    insertion sort

--- Sorting/sorting.py
+++ Sorting/sorting.py
@@ -5,7 +5,6 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] =arr[j+1], arr[j]
     print(arr)
 
-
 def selection_sort(arr):
     for i in range(len(arr)):
         min_index = i
@@ -14,5 +13,15 @@ def selection_sort(arr):
                 min_index = j
         arr[min_index], arr[i] = arr[i], arr[min_index]
     print(arr)
-input_arr = [6,1,2,5,4,9,8,7,3]
-selection_sort(input_arr)
+
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        min_index=i
+        for j in range(0,min_index):
+            if arr[i]<arr[j]:
+                arr.insert(j,arr.pop(i))
+    print(arr)
+
+
+input_arr = [6,1,2,5,4,9,8,7,3,-1,0,10]
+insertion_sort(input_arr)
