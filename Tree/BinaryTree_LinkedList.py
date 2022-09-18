eef808c238907b76cc4af5490bc28ec8f86c7327
mdshadanaltmash <mdshadanaltmash44@gmail.com>
2022-09-18 22:46:53 +0530

    leetcode practice

--- Tree/BinaryTree_LinkedList.py
+++ Tree/BinaryTree_LinkedList.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('/Users/mdshadanaltmash/Algorithms/Queue')
-from QueueUsingLinkedList import Queue
+from Queue.QueueUsingLinkedList import Queue
 
 class TreeNode:
     def __init__(self,data) -> None:
---
+++ spiralMatrix_leetcode54.py
@@ -0,0 +1,24 @@
+def spiral(matrix):
+    op = []
+    m,n = len(matrix),len(matrix[0])
+    x1, y1 = 0,0
+    x2 , y2 = n, m
+    while x1<x2 and y1<y2:
+        for j in range(x1,x2):
+            op.append(matrix[y1][j])
+        y1+=1
+        for i in range(y1,y2):
+            op.append(matrix[i][x2-1])
+        x2-=1
+        if not(x1<x2 and y1<y2):
+            break
+        for j in range(x2-1,x1-1,-1):
+            op.append(matrix[y2-1][j])
+        y2-=1
+        for i in range(y2-1,y1-1,-1):
+            op.append(matrix[i][x1])
+        x1+=1
+    return op
+a=[[6,9,7]]
+b=[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+print(spiral(b))
