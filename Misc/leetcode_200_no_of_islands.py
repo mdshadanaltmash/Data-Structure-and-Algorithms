951f44fa923fb1706c71eda8dc682c16afb8f258
mdshadanaltmash <mdshadanaltmash44@gmail.com>
2024-04-19 00:57:48 +0530

    leetcode 200 graph BFS, DFS problem

---
+++ Misc/leetcode_200_no_of_islands.py
@@ -0,0 +1,39 @@
+import collections
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        if not grid:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
+
+        def bfs(r, c):
+            q = collections.deque()
+            q.append((r,c))
+            visit.add((r,c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
+                for dr, dc in directions:
+                    r, c = row+dr, col+dc
+                    if (r in range(rows) 
+                        and c in range(cols)
+                        and grid[r][c]=='1'
+                        and (r,c) not in visit):
+                        q.append((r,c))
+                        visit.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in visit:
+                    bfs(r,c)
+                    islands+=1
+        
+        return islands
+    
+
+tc = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+obj = Solution()
+print(obj.numIslands(tc))
