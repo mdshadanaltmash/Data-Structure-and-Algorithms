6bf3d7008470319d0db1cd2f4ef105c89992d7b3
mdshadanaltmash <mdshadanaltmash44@gmail.com>
2025-02-23 12:22:54 +0530

    formatting

--- Misc/arr_inv_problem.py
+++ Misc/arr_inv_problem.py
@@ -1,55 +1,61 @@
 import time
+
+
 def array_inversion(arr):
-    temp_arr = [-1]*len(arr)
-    return count_inversion(arr, temp_arr, 0, len(arr)-1)
+    temp_arr = [-1] * len(arr)
+    return count_inversion(arr, temp_arr, 0, len(arr) - 1)
+
 
 def count_inversion(arr, temp_arr, left, right):
     inv_count = 0
-    if left<right:
-        mid =(left+right)//2
+    if left < right:
+        mid = (left + right) // 2
         inv_count += count_inversion(arr, temp_arr, left, mid)
-        inv_count += count_inversion(arr, temp_arr, mid+1, right)
+        inv_count += count_inversion(arr, temp_arr, mid + 1, right)
         inv_count += merge_and_count_inv(arr, temp_arr, left, mid, right)
     return inv_count
 
+
 def merge_and_count_inv(arr, temp_arr, left, mid, right):
     i = left
-    j = mid+1
+    j = mid + 1
     k = left
     c_inv = 0
 
-    while i<= mid and j <= right:
-        if arr[i]<= arr[j]:
+    while i <= mid and j <= right:
+        if arr[i] <= arr[j]:
             temp_arr[k] = arr[i]
-            i+=1
-            k+=1
+            i += 1
+            k += 1
         else:
-            temp_arr[k]= arr[j]
-            j+=1
-            k+=1
-            c_inv+=(mid-i+1)
-    
-    while i<= mid:
-        temp_arr[k]= arr[i]
-        i+=1
-        k+=1
-    
-    while j<= right:
-        temp_arr[k] = arr[j]
-        j+=1
-        k+=1
+            temp_arr[k] = arr[j]
+            j += 1
+            k += 1
+            c_inv += (mid - i + 1)
 
-    for x in range(left, right+1):
+    while i <= mid:
+        temp_arr[k] = arr[i]
+        i += 1
+        k += 1
+
+    while j <= right:
+        temp_arr[k] = arr[j]
+        j += 1
+        k += 1
+
+    for x in range(left, right + 1):
         arr[x] = temp_arr[x]
     return c_inv
 
-start_time= time.time()
-arr = []
-#arr = [1,20, 6, 4,5]
 
-with open('Misc/numbers.txt') as numbers:
-    for num in numbers:
-        arr.append(int(num))
-print(array_inversion(arr))
-end_time = time.time()
-print(round((end_time-start_time),3))
+if __name__ == '__main__':
+    start_time = time.time()
+    arr = []
+    #arr = [1,20, 6, 4,5]
+
+    with open('numbers.txt') as numbers:
+        for num in numbers:
+            arr.append(int(num))
+    print(array_inversion(arr))
+    end_time = time.time()
+    print(round((end_time - start_time), 3))
--- Missing_number.py
+++ Missing_number.py
@@ -1,12 +1,13 @@
 # find missing number between range 1 to n
 
 
-def missing_num(arr,n):
-    range_sum = (n*(n+1))/2
-    list_sum= sum(arr)
-    return (range_sum - list_sum)
-    
-n=100
-array = [i for i in range(n+1)]
+def missing_num(arr, n):
+    range_sum = (n * (n + 1)) / 2
+    list_sum = sum(arr)
+    return range_sum - list_sum
+
+
+n = 100
+array = [i for i in range(n + 1)]
 array.remove(78)
-print(missing_num(array,100))
+print(missing_num(array, 100))
